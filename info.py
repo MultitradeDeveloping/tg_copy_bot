02fd15099c4f1ee1a54fb6a22946c82f248c52ac
Multitrade <multitrade.crypto@gmail.com>
2025-05-25 00:26:49 +0300

    blyyattt nadeus last pzhpzh

--- info.py
+++ info.py
@@ -5,7 +5,7 @@ from _libs import *
 api_id = '15944941'
 api_hash = '272f6b2104875189e6ba63915e887065'
 bot_api = "7745895532:AAHlBR8tyr9SPAEU8qnIYNDFHCQLnowiX78"
-channel_toread_username = ["test_lize090", "bybit_tokensplash", "kormushka_mexc", "mexcTracker", "DCATrack", "","","","test_lize090"] #NOT USING @
+channel_toread_username = ["test_lize090", "bybit_tokensplash", "kormushka_mexc", "mexcTracker", "DCATrack", "infinityhedge","invest_zonaa","WatcherGuru","test_lize090"] #NOT USING @
 channel_tosend_username = ["-1002512059520","-1002512059520","-1002512059520","-1002512059520","-1002512059520","-1002512059520","-1002512059520","-1002512059520","-1002512059520"] #USING @
 topic_tosend_username = ["3", "26", "5", "3", "9", "2161", "2161","2161","3"]
 
